2b9c17eea0b3128f9d7144332887d17b4ea2bbe0
PhilAMartin <phil.martin.research@gmail.com>
2024-03-21 11:13:38 +0100

    updated data with new studies

--- code/data_extraction.R
+++ code/data_extraction.R
@@ -1,8 +1,15 @@
 #script for data extraction of a few papers
 
 library(metaDigitise)
+library(shinyDigitise)
+
 
 metaDigitise("data/data_extraction/Ashton_2019/")
 
 Ashton_2019_data<-metaDigitise(dir="data/data_extraction/Ashton_2019/")
 write.csv(Ashton_2019_data,file = "data/data_extraction/Ashton_2019/Ashton_2019_figures.csv")
+
+
+#extract data for Zhou_2023
+Zhou_data<-shinyDigitise("C:/Users/phil.martin/Desktop/Zhou_2023")
+
